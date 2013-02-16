a67d9860f91bed427027cf9e1fac57c82f4df60a
Adrien BATAILLE <adrien.bataille1@gmail.com>
2013-02-16 08:44:21 +0100

    [ iCommerce ] Workspace - Ajout Classes Article et Parser
    - Ajout des classes articles et parser dans le projet

---
+++ iCommerce/iCommerce/Article.h
@@ -0,0 +1,19 @@
+//
+//  Article.h
+//  XmlParse
+//
+//  Created by Adrien BATAILLE on 02/02/13.
+//  Copyright (c) 2013 Adrien BATAILLE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Article : NSObject
+
+@property (strong, nonatomic)NSString *refArticle;
+@property (strong, nonatomic)NSString *nomArticle;
+@property (strong, nonatomic)NSString *description;
+@property (strong, nonatomic)NSString *prixHT;
+@property (strong, nonatomic)NSString *tauxTVA;
+
+@end
---
+++ iCommerce/iCommerce/Article.m
@@ -0,0 +1,19 @@
+//
+//  Article.m
+//  XmlParse
+//
+//  Created by Adrien BATAILLE on 02/02/13.
+//  Copyright (c) 2013 Adrien BATAILLE. All rights reserved.
+//
+
+#import "Article.h"
+
+@implementation Article
+
+@synthesize refArticle = _refArticle;
+@synthesize nomArticle = _nomArticle;
+@synthesize description = _description;
+@synthesize prixHT = _prixHT;
+@synthesize tauxTVA = _tauxTVA;
+
+@end
